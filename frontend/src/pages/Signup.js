b07cd5f35675c912f0470460a3500eaab4993093
AlisyaAfiz <23018913@myrp.edu.sg>
2025-02-01 20:38:40 +0800

    Update

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,15 +1,20 @@
 import {useState} from 'react'
 import {useSignup} from '../hooks/useSignup'
+import { useHistory } from 'react-router-dom';
 
 const Signup = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const {signup, error, isLoading} = useSignup()
+    const history = useHistory();
 
     const handleSubmit = async (e) => {
         e.preventDefault()
 
-        await signup(email, password)
+        const result = await signup(email, password);
+        if (result) {
+            history.push('/'); 
+        }
     }
 
     return (
